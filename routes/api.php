4b550b5db45b206a41dbef99787f7db5b8df46e5
azza <azzahajmahmoud7@gmail.com>
2025-07-02 00:13:32 +0300

    fix: UrunlerController added and tracked by Git

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\Http\Controllers\UrunlerController;
-
-use App\Http\Controllers\UrunlerController;
-
-Route::post('/urunler', [UrunlerController::class, 'store']);
-Route::put('/urunler/{id}', [UrunlerController::class, 'update']);
-Route::delete('/urunler/{id}', [UrunlerController::class, 'destroy']);
